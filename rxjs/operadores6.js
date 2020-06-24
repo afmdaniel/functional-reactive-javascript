0f2c1dad5f6f28dbbb6740aac5296c244f559d45
afmdanielwin <danielfarias380@gmail.com>
2020-06-24 13:22:34 -0300

    Create Pipeble Operator Desafio 3

--- rxjs/operadores6.js
+++ rxjs/operadores6.js
@@ -1,24 +1,44 @@
 const { from, Observable } = require('rxjs')
 
-function createPipebleOpertor(nextGenerator) {
+function createPipebleOpertor(operator) {
     return function(sourceObservable) {
         return Observable.create(subscriber => {
-            sourceObservable.subscribe({
-                next: nextGenerator(subscriber)
-            })
+            sourceObservable.subscribe(operator(subscriber))
         })
     }
 }
 
 function primeiro() {
-    return createPipebleOpertor(subscriber => value => {
-        subscriber.next(value)
-        subscriber.complete()
-    })
+    return createPipebleOpertor(subscriber => ({
+        next(value) {
+            subscriber.next(value)
+            subscriber.complete()
+        }
+    }))
 }
 
+function nenhum() {
+    return createPipebleOpertor(subscriber => ({
+        next(value) {
+            subscriber.complete()
+        }
+    }))
+}
+
+function ultimo() {
+    return createPipebleOpertor(subscriber => ({
+        next(v) {
+            ultimo = v
+        },
+        complete() {
+            subscriber.next(ultimo)
+            subscriber.complete()
+        }
+    }))
+}
 
 from([1, 2, 3, 4, 5])
-.pipe(primeiro())
-// .pipe(ultimo())
+// .pipe(primeiro())
+.pipe(ultimo())
+// .pipe(nenhum())
 .subscribe(console.log)
